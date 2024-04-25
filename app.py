b3feecac8d8408d36f85f4c160908ad2b88e8c2b
Henry Navntoft <henrylnavntoft@gmail.com>
2024-04-25 13:08:08 +0200

    "..."

---
+++ app.py
@@ -0,0 +1,25 @@
+#########################
+from bottle import default_app, get, post, run
+import git
+ 
+@post('/secret_url_for_git_hook')
+def git_update():
+  repo = git.Repo('./mysite')
+  origin = repo.remotes.origin
+  repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+  origin.pull()
+  return ""
+ 
+ 
+##############################
+@get("/")
+def _():
+  return "One"
+ 
+##############################
+try:
+  import production
+  application = default_app()
+except Exception as ex:
+  print("Running local server")
+  run(host="127.0.0.1", port=80, debug=True, reloader=True)
